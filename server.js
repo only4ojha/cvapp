17670b86600749e380eed895fc595aa7145a17dc
only4ojha <ojha@ymail.com>
2020-04-23 13:56:46 +0530

    SSL

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const fs = require('fs');
+const key = fs.readFileSync('key.pem');
+const cert = fs.readFileSync('cert.pem');
 const express = require('express');
-const server = require('http').createServer();
+const https = require('https');
 const app = express();
-const port = 4000;
+var path = require('path');
+const server = https.createServer({key: key, cert: cert }, app);
+const port = 3000;
 const io = require('socket.io')(server, { wsEngine: 'ws', origins:'*:*' });
 
-
 // Set public folder as root
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Provide access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
@@ -14,7 +18,4 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.info('listening on %d', port);
-});
+https.createServer({key: key, cert: cert }, app).listen(port);
